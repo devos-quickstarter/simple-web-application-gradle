c6bc72cc04d1489024b6dbd133dcb83f576aeff7
gayeon.lee <gayeon.lee@bespinglobal.com>
2022-12-21 18:32:22 +0900

    pipeline simple web app catalog 예제 source commit

---
+++ src/main/java/com/bespin/demo/ums/SimpleMvcWithoutdbGradleApplication.java
@@ -0,0 +1,13 @@
+package com.bespin.demo.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMvcWithoutdbGradleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleMvcWithoutdbGradleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bespin/demo/ums/SimpleMvcWithoutdbGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bespin.demo.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMvcWithoutdbGradleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
